4ff90bd7b5c35d7c67a5c65d0e91501956d63581
romil rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2016-02-08 10:58:58 +0200

    added inital singleton pattern layout

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
- 
+(function(){
+   "use strict";
+
+   var Moosipurk = function(){
+
+     // SEE ON SINGLETON PATTERN
+     if(Moosipurk.instance){
+       return Moosipurk.instance;
+     }
+     //this viitab Moosipurk fn
+     Moosipurk.instance = this;
+
+     console.log('moosipurgi sees');
+
+     // Kui tahan Moosipurgile referenci siis kasutan THIS = MOOSIPURGI RAKENDUS ISE
+     this.init();
+   };
+
+
+   // Kõik funktsioonid lähevad Moosipurgi külge
+   Moosipurk.prototype = {
+
+     init: function(){
+       console.log('Rakendus läks tööle');
+     },
+
+     bindMouseEvent: function(){
+
+     }
+
+   };
+
+   var app = new Moosipurk();
+
+})();
